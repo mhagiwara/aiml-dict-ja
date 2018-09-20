3ff66105769be2425c63d22aca3492cf85584944
Masato Hagiwara <masato@duolingo.com>
2018-09-19 21:52:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# AIML-dict-ja ― オープンソースの AI (人工知能)・ML (機械学習) 用語辞典
+
+AIML-dict-ja は、オープンソースの人工知能・機械学習分野の英語→日本語用語辞典です。[クリエイティブ・コモンズ表示 - 継承 3.0 非移植 (CC BY-SA 3.0) ライセンス](https://creativecommons.org/licenses/by-sa/3.0/deed.ja) の下で利用できます。
+
+メインの辞書ファイルは [aiml-dict-ja.yml](aiml-dict-ja.yml) です。YAML 形式で書かれています。
+
+[validate.py](validate.py) は、辞書ファイル `aiml-dict-ja.yml` が、決められた構造に従っているかどうか検査するスクリプトです。実行するには `PyYAML` パッケージが必要です。構造に問題がなければ、 `Validation: PASSED` を出力します。
---
+++ validate.py
@@ -0,0 +1,42 @@
+import yaml
+
+
+MANDATORY_FIELDS = ['word', 'trans', 'kana']
+OPTIONAL_FIELDS = ['var', 'def', 'ref', 'syn']
+
+
+def validate_entry(entry):
+    if not isinstance(entry, dict):
+        raise ValueError('Entry %r is not a dict' % entry)
+
+    for field in MANDATORY_FIELDS:
+        if  field not in entry:
+            raise ValueError('Entry %r does not have the mandatory `%s` field' % (entry, field))
+
+    unrecognized_fields = set(entry.keys()) - set(MANDATORY_FIELDS) - set(OPTIONAL_FIELDS)
+    if unrecognized_fields:
+        raise ValueError('Entry %r has unrecognized field(s): %r' % (entry, unrecognized_fields))
+
+    if 'ref' in entry:
+        if not isinstance(entry['ref'], list):
+            raise ValueError('Entry %r has a `ref` field which is not a list' % entry)
+
+
+def main():
+    with open('aiml-dict-ja.yml') as f:
+        data = yaml.load(f)
+
+        if not isinstance(data, list):
+            raise ValueError('The dictionary data is not a list')
+
+        print('%d entries found.' % len(data))
+
+        for entry in data:
+            print(entry)
+            validate_entry(entry)
+
+        print('Validation: PASSED')
+
+
+if __name__ == '__main__':
+    main()
